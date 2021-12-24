bbcf8cf1671a8bf7eee07982b7e9c1e87f8adaff
axlopezre <83840788+axlopezre@users.noreply.github.com>
2021-12-23 23:12:35 -0700

    ServiciosAgregado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { ServiciosInstitucionalesComponent } from './pages/servicios/servicios-institucionales/servicios-institucionales.component';
 import { ServiciosEducativosComponent } from './pages/servicios/servicios-educativos/servicios-educativos.component';
 import { PrincipalPlataformaComponent } from './pages/Plataforma/principal-plataforma/principal-plataforma.component';
+import { ServiciosAdministrativosComponent } from './pages/servicios/servicios-administrativos/servicios-administrativos.component';
+import { ServiciosExternosComponent } from './pages/servicios/servicios-externos/servicios-externos.component';
 import { UsuariosComponent} from './pages/Plataforma/usuarios/usuarios.component';
 import { PerfilComponent} from './pages/Plataforma/perfil/perfil.component';
 import { CursoComponent} from './pages/Plataforma/curso/curso.component';
@@ -22,6 +24,8 @@ const routes: Routes =[
   {path: '', component: HomeComponent},
   {path: 'servicios-institucionales',component: ServiciosInstitucionalesComponent},
   {path: 'servicios-educativos',component: ServiciosEducativosComponent},
+  {path: 'servicios-administrativos',component: ServiciosAdministrativosComponent},
+  {path: 'servicios-externos',component: ServiciosExternosComponent},
   {path: 'login', component: LoginComponent},
   {path: 'contacts', component: ContactsComponent},
   {path: 'principal-plataforma', component: PrincipalPlataformaComponent},
